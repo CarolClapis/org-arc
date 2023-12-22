b256a39292978ef7f41ea3880ac485baf1c18643
Caroline <145502020+CarolClapis@users.noreply.github.com>
2023-12-22 00:02:28 -0300

    Create Hello_World.asm

---
+++ Hello_World/Hello_World.asm
@@ -0,0 +1,66 @@
+jmp main
+
+
+mensagem : var #21
+static mensagem + #0, #'A'
+static mensagem + #1, #'A'
+static mensagem + #2, #'A'
+static mensagem + #3, #'a'
+static mensagem + #4, #'B'
+static mensagem + #5, #'C'
+static mensagem + #6, #'E'
+static mensagem + #7, #'N'
+static mensagem + #8, #'T'
+static mensagem + #9, #'E'
+static mensagem + #10, #'R'
+static mensagem + #11, #' '
+static mensagem + #12, #'T'
+static mensagem + #13, #'O'
+static mensagem + #14, #' '
+static mensagem + #15, #'S'
+static mensagem + #16, #'T'
+static mensagem + #17, #'A'
+static mensagem + #18, #'R'
+static mensagem + #19, #'T'
+static mensagem + #20, #'\0'
+
+
+mensagem2 : string "Hello, world!"
+
+
+main:
+	loadn r0, #0		
+	loadn r1, #mensagem2	
+	loadn r2, #256		
+	
+	call Imprimestr   
+
+	halt
+	
+	
+Imprimestr:	
+	push r0	
+	push r1	
+	push r2
+	push r3	
+	push r4	
+	
+	loadn r3, #'\0'	; Criterio de parada
+
+ImprimestrLoop:	
+	loadi r4, r1
+	cmp r4, r3
+	jeq ImprimestrSai
+	add r4, r2, r4
+	outchar r4, r0
+	inc r0
+	inc r1
+	jmp ImprimestrLoop
+	
+ImprimestrSai:	
+	pop r4	
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts
